45b0a6b809e495704b39bad268cf89e4cdfe9f3a
CEOs-Hub <125826240+CEOs-Hub@users.noreply.github.com>
2023-03-26 13:43:42 +0200

    Create Copy-Hwid.lua

---
+++ Copy-Hwid.lua
@@ -0,0 +1,1 @@
+local a=request or syn and syn.request or http and http.request or nil;local b=a({Url='https://httpbin.org/get',Method='GET'}).Body;local c=game:GetService('HttpService'):JSONDecode(b)hwid="idk"hwid_list={"Syn-Fingerprint","Sw-Fingerprint","Krnl-Hwid"}for d,e in next,hwid_list do if c.headers[e]then hwid=c.headers[e]break end end;setclipboard(tostring(hwid))
