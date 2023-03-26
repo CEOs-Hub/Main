4e6cd6b03e5443c919380f7249cf01912a2ff696
CEOs-Hub <125826240+CEOs-Hub@users.noreply.github.com>
2023-03-26 13:46:03 +0200

    Update Copy-Hwid.lua

--- Copy-Hwid.lua
+++ Copy-Hwid.lua
@@ -1,1 +1,6 @@
 local a=request or syn and syn.request or http and http.request or nil;local b=a({Url='https://httpbin.org/get',Method='GET'}).Body;local c=game:GetService('HttpService'):JSONDecode(b)hwid="idk"hwid_list={"Syn-Fingerprint","Sw-Fingerprint","Krnl-Hwid"}for d,e in next,hwid_list do if c.headers[e]then hwid=c.headers[e]break end end;setclipboard(tostring(hwid))
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "CEOs Hub", -- Required
+	Text = "Copied Hwid\nYou must be using Syn, Krnl or ScriptWare", -- Required
+	Icon = "rbxassetid://1234567890" -- Optional
+})
